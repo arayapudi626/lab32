e60d0e6d36505f2834472688edbe0607d357cf4f
arayapudi626 <rayapudianisha@gmail.com>
2025-11-13 15:00:25 -0800

    Milestone 1

---
+++ lab32.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include "Car.h"
+using namespace std;
+
+const int CAR = 2;
+int main(){
+//milestone 1:
+srand(time(0));
+deque<Car> tollBooth;
+
+for (int i = 0; i < CAR; i++){
+    tollBooth.push_back(Car());
+}
+
+//milestone 2:
+
+
+}
