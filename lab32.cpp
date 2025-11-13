43f450de5cf70e0505fe187150060769b52fa698
arayapudi626 <rayapudianisha@gmail.com>
2025-11-13 15:20:17 -0800

    update

--- lab32.cpp
+++ lab32.cpp
@@ -27,18 +27,29 @@ while(!tollBooth.empty()){
     int random = rand() % 100;
 
     if (random < 55){
-        cout << "Time: " << time << "Operation: Car paid: ";
+        cout << "Time: " << time << " Operation: Car paid: ";
         tollBooth.front().print();
         tollBooth.pop_front();
     }
     else{
         Car newCar;
-        cout << "Time: " << time << "Operation: Joined lane: ";
+        cout << "Time: " << time << " Operation: Joined lane: ";
         newCar.print();
         tollBooth.push_back(newCar);
     }
+
+//print
+cout << "Queue: " << endl;
+if (!tollBooth.empty()) {
+    for (int i = 0; i < tollBooth.size(); i++){
+        tollBooth[i].print();
+    }
+} else {
+    cout << "Empty" << endl;
 }
+cout << endl;
+time++;
 
 
-
+}
 }
