0ea08860e240963f6e88bcfddf97032de74cbe7f
arayapudi626 <rayapudianisha@gmail.com>
2025-11-16 14:21:41 -0800

    update

--- lab33.cpp
+++ lab33.cpp
@@ -65,13 +65,16 @@ for (int t = 1; t <= 20; t++){
                 Car c;
                 cout << "Lane: " << (i+1) << " Joined: ";
                 c.print();
-                lanes[i].pop_back();
+                lanes[i].push_back(c);
             }
             else {
                 Car m = lanes[i].back();
                 lanes[i].pop_back();
 
+                int d = i;
+                while (d==i){
                 int d = rand() % LANES;
+                }
                 lanes[d].push_back(m);
                 cout << "Lane: " << (i+1) << " Switched: ";
                 m.print();
@@ -88,42 +91,4 @@ for (int t = 1; t <= 20; t++){
     }
     return 0;
 
-
-
-
-
-
-
 }
-
-/*
-while(!tollBooth.empty()){
-    int random = rand() % 100;
-
-    if (random < 55){
-        cout << "Time: " << time << " Operation: Car paid: ";
-        tollBooth.front().print();
-        tollBooth.pop_front();
-    }
-    else{
-        Car newCar;
-        cout << "Time: " << time << " Operation: Joined lane: ";
-        newCar.print();
-        tollBooth.push_back(newCar);
-    }
-
-//print
-cout << "Queue: " << endl;
-if (!tollBooth.empty()) {
-    for (int i = 0; i < tollBooth.size(); i++){
-        tollBooth[i].print();
-    }
-} else {
-    cout << "Empty" << endl;
-}
-cout << endl;
-time++;
-
-
-}
-} */
